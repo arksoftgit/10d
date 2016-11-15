b08f845a441678bdce2adc900865a4ad6953b4eb
arksoftgit <arksoft@comcast.net>
2016-11-15 10:37:17 -0500

    Set up command line build (using devenv.exe)

---
+++ a/zdr/MakeDevenv.bat
@@ -0,0 +1,10 @@
+"d:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ZDr.sln /build Debug /project JpegLib %1 %2 %3 %4
+"d:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ZDr.sln /build Debug /project ZDrApp %1 %2 %3 %4
+"d:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ZDr.sln /build Debug /project ZdCtl %1 %2 %3 %4
+"d:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ZDr.sln /build Debug /project TzCtl %1 %2 %3 %4
+"d:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ZDr.sln /build Debug /project TzAxCtl %1 %2 %3 %4
+"d:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ZDr.sln /build Debug /project Z2MP %1 %2 %3 %4
+"d:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ZDr.sln /build Debug /project ZSEMail %1 %2 %3 %4
+"d:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ZDr.sln /build Debug /project truegrid %1 %2 %3 %4
+"d:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ZDr.sln /build Debug /project tzvmledt %1 %2 %3 %4
+"d:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ZDr.sln /build Debug /project ZDr %1 %2 %3 %4
