9c514f031bb83fd67a0267f2f746775429bfff1d
arksoftgit <arksoft@comcast.net>
2016-11-16 17:34:49 -0500

    MakeDevenv.bat remarked projects that should be built by makeall ... not in the IDE

--- a/zdr/MakeDevenv.bat
+++ a/zdr/MakeDevenv.bat
@@ -5,6 +5,6 @@
 "d:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ZDr.sln /build Debug /project TzAxCtl %1 %2 %3 %4
 "d:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ZDr.sln /build Debug /project Z2MP %1 %2 %3 %4
 "d:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ZDr.sln /build Debug /project ZSEMail %1 %2 %3 %4
-"d:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ZDr.sln /build Debug /project truegrid %1 %2 %3 %4
-"d:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ZDr.sln /build Debug /project tzvmledt %1 %2 %3 %4
+rem "d:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ZDr.sln /build Debug /project truegrid %1 %2 %3 %4
+rem "d:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ZDr.sln /build Debug /project tzvmledt %1 %2 %3 %4
 "d:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" ZDr.sln /build Debug /project ZDr %1 %2 %3 %4
